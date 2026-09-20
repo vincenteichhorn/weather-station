a13a699b63e28add5638fbf992aff27f235c7567
Vincent Eichhorn <vincent.eichhorn@student.hpi.de>
2026-09-20 12:54:07 +0200

    fix deinstall

--- backend/deinstall.sh
+++ backend/deinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-APP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/backend/weather-backend
+APP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/backend/
 SERVICE_NAME=$(grep -oP '^SERVICE_NAME=\K.*' "${APP_DIR}/.env")
 
 sudo systemctl stop ${SERVICE_NAME}
